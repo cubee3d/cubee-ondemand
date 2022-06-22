46392103dd4356815675a8f4b41dc1d0a536c74b
LidorCubee3D <Lidor@cubee3d.com>
2022-06-22 21:43:41 +0300

    fixes

--- src/cmps/StepWelcomeFile.js
+++ src/cmps/StepWelcomeFile.js
@@ -13,7 +13,7 @@ export const StepWelcomeFile = ({ onFirstFileSelect, isLoading, apiKey }) => {
     useEffect(()=>{
         setTimeout(()=>{
             // document.location.ancestorOrigins[0])
-            window.parent.postMessage('Hello', 'http://localhost')
+            window.parent.postMessage('Hello', 'http://localhost/newwpsite/13-2/')
             // var event = new CustomEvent('myCustomEvent', { detail: {a: 1} })
             // window.parent.document.dispatchEvent(event)
         },3000)
